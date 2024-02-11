1d495a9f96b321603a098bba48c585d9fa300483
Alexander Blomberg <info@dalasoftware.se>
2024-02-11 13:29:23 +0100

    Add collection details API with ModelSerializer

--- store/views.py
+++ store/views.py
@@ -8,7 +8,7 @@ from .serializers import ProductSerializer
 @api_view()
 def product_list(request):
     queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 @api_view()
@@ -16,4 +16,8 @@ def product_detail(request, id):
     product = get_list_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
     
