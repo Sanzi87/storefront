2e612c9d1663d29f86639fe958df6965a8a70d01
Alexander Blomberg <info@dalasoftware.se>
2024-02-12 14:28:42 +0100

    Add pagination to Products view

--- store/views.py
+++ store/views.py
@@ -5,8 +5,9 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from store.filters import ProductFilter
+from .filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
+from .pagination import DefaultPagination
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
@@ -15,6 +16,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
     
