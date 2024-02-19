b557c0a8f816a2fa617cae239c5e518a64471b10
Alexander Blomberg <info@dalasoftware.se>
2024-02-19 11:02:19 +0100

    Implement Get, Post, Patch, Delete functions of CartItems

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin, messages
 from django.db.models.aggregates import Count
 from django.db.models.query import QuerySet
-from .models import Collection, Product, Customer, Order, OrderItem
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
+from . import models
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -19,7 +19,8 @@ class InventoryFilter(admin.SimpleListFilter):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
 
-@admin.register(Product)
+
+@admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
     prepopulated_fields = {
@@ -30,7 +31,7 @@ class ProductAdmin(admin.ModelAdmin):
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
-    list_per_page = 20
+    list_per_page = 10
     list_select_related = ['collection']
     search_fields = ['title']
 
@@ -49,11 +50,11 @@ class ProductAdmin(admin.ModelAdmin):
         self.message_user(
             request,
             f'{updated_count} products were successfully updated.',
-            messages.INFO
+            messages.ERROR
         )
-    
 
-@admin.register(Collection)
+
+@admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     autocomplete_fields = ['featured_product']
     list_display = ['title', 'products_count']
@@ -71,40 +72,43 @@ class CollectionAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            products_count=Count('product')
+            products_count=Count('products')
         )
 
 
-@admin.register(Customer)
+@admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership', 'orders']
+    list_display = ['first_name', 'last_name',  'membership', 'orders']
     list_editable = ['membership']
-    list_per_page = 20
+    list_per_page = 10
     ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders(self, customer):
-        url = (reverse('admin:store_order_changelist')
-               + '?'
-               + urlencode({
-                   'customer__id': str(customer.id)
-               }))
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
         return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             orders_count=Count('order')
         )
- 
+
+
 class OrderItemInline(admin.TabularInline):
     autocomplete_fields = ['product']
     min_num = 1
     max_num = 10
-    model = OrderItem
+    model = models.OrderItem
     extra = 0
-        
-@admin.register(Order)
+
+
+@admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ['customer']
     inlines = [OrderItemInline]
